f39695d5773e0a533cb5bd2a043cc858703c5a17
Shane Keulen <shane@devvirtual.dashboard.local>
2015-02-12 14:37:46 -0500

    juggling async done

--- jugglingasync.js
+++ jugglingasync.js
@@ -4,23 +4,31 @@ var urls = process.argv.slice(2);
 var count = 0;
 var strings = ['','',''];
 
-urls.forEach(function(url, index) {
-  function getRes(res) {
-    res.setEncoding('utf8');
+function getResponses(urls) {
+  urls.forEach(function(url, index) {
+    // Define the call behaviour
+    function getRes(res) {
+      res.setEncoding('utf8');
 
-    res.on('data', function(data) {
-      strings[index] += data;
-    });
+      res.on('data', function(data) {
+        strings[index] += data;
+      });
 
-    res.on('end', function() {
-      count++;
-      if (count == 3)
-        strings.forEach(function(stringData) {
-          console.log(stringData);
-        });
-    });
+      res.on('end', function() {
+        count++;
+        if (count == 3)
+          strings.forEach(function(stringData) {
+            console.log(stringData);
+          });
+      });
 
-  };
+    };
+    http.get(url, getRes);
+  });
+}
 
-  http.get(url, getRes);
-});
+getResponses(urls);
+
+// This would be easier to reason about with promises.
+// make the array of 3 equal to the result of the promises,
+// and call a then on the array of promeises?
