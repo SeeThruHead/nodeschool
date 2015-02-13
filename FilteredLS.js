8475f821ab54c6cf200e9dd71b213b09c17080f9
Shane Keulen <STH@sthbk.lan>
2015-02-12 22:14:05 -0500

    fixed filteredLS

--- FilteredLS.js
+++ FilteredLS.js
@@ -1,7 +1,19 @@
-var      fs       = require('fs'),
-  filterExt       = process.argv[3],
-  directory       = process.argv[2];
-  filterFileNames = require('./mymodule')
+var      fs = require('fs'),
+  filterExt = process.argv[3],
+  directory = process.argv[2];
 
+function filterFileNames(err, fileNames) {
+  // console.log(fileNames);
+	var result = fileNames.filter(function(fileName) {
+    // console.log(fileName);
+		var ext = fileName.slice(fileName.length - filterExt.length - 1);
+    // console.log(ext);
+    return (ext === ('.' + filterExt));
+    
+	});
+  result.forEach(function(fileName) {
+    console.log(fileName);
+  });
+}
 
 fs.readdir(directory, filterFileNames);
